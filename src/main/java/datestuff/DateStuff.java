eff438d43fa44d66571f9def1387cc15f51d9c13
Michael Allen <mallen@cccis.com>
2023-02-03 08:48:00 -0600

    testing some features

--- src/main/java/datestuff/DateStuff.java
+++ src/main/java/datestuff/DateStuff.java
@@ -1,8 +1,12 @@
 package datestuff;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateStuff {
     private static final String FILE_PATTERN = "'NadaChromeMap_'yyyyMMM'.zip'";
@@ -10,5 +14,27 @@ public class DateStuff {
     static String nadaChromMapFtpFileName(Date date) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(FILE_PATTERN);
         return simpleDateFormat.format(date);
+    }
+
+    static String lossDateMinus90(LocalDate lossDate) {
+        GregorianCalendar calLossDate = new GregorianCalendar();
+
+        //String calLossDateStr = fmt.format(calLossDate.getTime());
+        String lossDateStr = DateTimeFormatter.ofPattern("MMMM dd, yyyy").format(lossDate);
+        System.out.println("lossDateStr =" + lossDateStr);
+        LocalDate lossDateMinus90 = lossDate.minusDays(90);
+        String lossDateMinus90Str = DateTimeFormatter.ofPattern("MMMM dd, yyyy").format(lossDateMinus90);
+        return lossDateMinus90Str;
+    }
+
+    static Date makeDateFromString(String dateStr) {
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = fmt.parse(dateStr);
+        } catch (ParseException e) {
+            System.out.println("Whoa... bummer, man: can't parse " + dateStr);
+        }
+        return date;
     }
 }
--- src/test/java/datestuff/DateStuffTest.java
+++ src/test/java/datestuff/DateStuffTest.java
@@ -2,6 +2,8 @@ package datestuff;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,11 +13,20 @@ public class DateStuffTest {
     @Test
     public void nadaChromMapFtpFileName() {
         Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, 11);
         cal.set(Calendar.MONTH, Calendar.JUNE);
         cal.set(Calendar.YEAR, 2020);
         Date date = cal.getTime();
 
         String strDate = DateStuff.nadaChromMapFtpFileName(date);
         assertEquals("NadaChromeMap_2020Jun.zip", strDate);
+
+    }
+
+    @Test
+    public void dateMinus90Tesyt() {
+        LocalDate localDate = LocalDate.of(2022, Month.NOVEMBER, 20);
+        String dateMinus90 = DateStuff.lossDateMinus90(localDate);
+        assertEquals("August 22, 2022", dateMinus90);
     }
 }
